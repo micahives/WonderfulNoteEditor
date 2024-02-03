561549de63e761a37248f95ce76c65a24485b6c4
Micah Ives <micbenives@gmail.com>
2024-02-03 16:28:08 -0600

    Update webpack config for css loader/pwa manifest, update package.json references

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -18,13 +15,46 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+      }),
+      new WebpackPwaManifest({
+        name: 'Your App Name',
+        short_name: 'App',
+        description: 'Description of your app',
+        background_color: '#ffffff',
+        crossorigin: 'anonymous',
+        icons: [
+          {
+            src: path.resolve('./src/images/logo.png'), 
+            sizes: [96, 128, 192, 256, 384, 512] 
+          }
+        ]
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'sw.js'
+      })
     ],
 
     module: {
       rules: [
-        
-      ],
-    },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-transform-runtime', '@babel/plugin-proposal-object-rest-spread']
+            }
+          }
+        }
+      ]
+    }
   };
 };
